54e174f7b5e80e0a2c2520b2896b3344c7d44d7d
Emre_Ugurlu <Emre_Ugurlu@192.168.0.8>
2018-11-12 02:14:00 -0500

    new sql table inculde course tabla

--- CSI518_team5/sql/database3.sql
+++ CSI518_team5/sql/database3.sql
@@ -1,53 +1,31 @@
-CREATE DATABASE  IF NOT EXISTS `csi518_school_sys` /*!40100 DEFAULT CHARACTER SET latin1 */;
+UNLOCK TABLES;
+
+DROP database IF EXISTS `csi518_school_sys`;
+
+DROP user IF EXISTS `team5`;
+DROP user IF EXISTS `team5`@`localhost`;
+CREATE user `team5`@`localhost` IDENTIFIED BY 'icsi518';
+
+CREATE DATABASE `csi518_school_sys` /*!40100 DEFAULT CHARACTER SET latin1 */;
+GRANT ALL PRIVILEGES ON `csi518_school_sys`.* TO `team5`@`localhost`;
+
 USE `csi518_school_sys`;
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: localhost    Database: csi518_school_sys
--- ------------------------------------------------------
--- Server version	5.7.21
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `role`
---
-
-DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `idroles` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idroles`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `role`
---
 
 LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
 INSERT INTO `role` VALUES (1,'admin'),(2,'teacher'),(3,'student'),(4,'parent');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `user` (
   `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -58,25 +36,27 @@ CREATE TABLE `user` (
   `role` int(11) NOT NULL,
   PRIMARY KEY (`iduser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
 
 LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin',NULL,NULL,NULL,'password',1),(2,'eu664771','emre','ugurlu','eugurlu@albany.edu','password',3),(3,'ahmet123','ahmet','cesur','a@gmail.com','password',3),(4,'mustafa123','mustafa','ugurlu','mu@gmail.com','password',3),(5,'pranith','Pranith','Dachepally','pdachepally@albany.edu','1234',3);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin',NULL,NULL,NULL,'password',1),(2,'eu664771','emre','ugurlu','eugurlu@albany.edu','password',3),(3,'pranith','Pranith','Dachepally','pdachepally@albany.edu','1234',3);
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+DROP TABLE IF EXISTS `course`;
 
--- Dump completed on 2018-10-11 14:10:20
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `course` (
+  `course_id` varchar(10) NOT NULL,
+  `course_name` varchar(45) DEFAULT NULL,
+  `course_capcity` int(11) DEFAULT NULL,
+  `instructor` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`course_id`),
+  UNIQUE KEY `course_id_UNIQUE` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+LOCK TABLES `course` WRITE;
+INSERT INTO `course` VALUES ('CSI500','Operating System',30,'John'),('CSI518','Software Engineering',30,'George'),('CSI535','Aritificial Intelligence',50,'John');
+UNLOCK TABLES;
+
