3e92e5d36b1d9a050cfef87f7a8ee2714172851d
xkzzxx627 <42773393+xkzzxx627@users.noreply.github.com>
2018-12-04 04:32:42 -0500

    class table added.

--- CSI518_team5/sql/database3.sql
+++ CSI518_team5/sql/database3.sql
@@ -1,58 +1,136 @@
-UNLOCK TABLES;
-
-DROP database IF EXISTS `csi518_school_sys`;
-
-DROP user IF EXISTS `team5`;
-DROP user IF EXISTS `team5`@`localhost`;
-CREATE user `team5`@`localhost` IDENTIFIED BY 'icsi518';
-
-CREATE DATABASE `csi518_school_sys` /*!40100 DEFAULT CHARACTER SET latin1 */;
-GRANT ALL PRIVILEGES ON `csi518_school_sys`.* TO `team5`@`localhost`;
-
+CREATE DATABASE  IF NOT EXISTS `csi518_school_sys` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `csi518_school_sys`;
+-- MySQL dump 10.13  Distrib 8.0.12, for macos10.13 (x86_64)
+--
+-- Host: localhost    Database: csi518_school_sys
+-- ------------------------------------------------------
+-- Server version	8.0.12
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `role` (
-  `idroles` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idroles`)
+--
+-- Table structure for table `apt`
+--
+
+DROP TABLE IF EXISTS `apt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `apt` (
+  `receiverid` int(11) NOT NULL,
+  `rfisrtname` varchar(45) NOT NULL,
+  `rlastname` varchar(45) NOT NULL,
+  `senderid` int(11) NOT NULL,
+  `sfirstname` varchar(45) NOT NULL,
+  `slastname` varchar(45) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `apt`
+--
 
-LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'admin'),(2,'teacher'),(3,'student'),(4,'parent');
+LOCK TABLES `apt` WRITE;
+/*!40000 ALTER TABLE `apt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `apt` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `class`
+--
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `class` (
+  `idclass` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idclass`),
+  KEY `course_id_idx` (`course_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `user` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `firstname` varchar(45) DEFAULT NULL,
-  `lastname` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `password` varchar(45) NOT NULL,
-  `role` int(11) NOT NULL,
-  PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+--
+-- Dumping data for table `class`
+--
 
-LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES (1,'admin',NULL,NULL,NULL,'password',1),(2,'eu664771','emre','ugurlu','eugurlu@albany.edu','password',3),(3,'pranith','Pranith','Dachepally','pdachepally@albany.edu','1234',3);
-
+LOCK TABLES `class` WRITE;
+/*!40000 ALTER TABLE `class` DISABLE KEYS */;
+INSERT INTO `class` VALUES (1,1,3);
+/*!40000 ALTER TABLE `class` ENABLE KEYS */;
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS 'msg';
+--
+-- Table structure for table `course`
+--
 
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(45) NOT NULL,
+  `course_capcity` int(11) NOT NULL,
+  `instructor` int(11) NOT NULL,
+  PRIMARY KEY (`course_id`),
+  UNIQUE KEY `course_id_UNIQUE` (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES (1,'CSI500-Operating System',30,2),(2,'CSI518-Software Engineering',30,2),(3,'CSI535-Aritificial Intelligence',50,4);
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `msg`
+--
+
+DROP TABLE IF EXISTS `msg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `msg` (
   `from` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `content` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `msg`
+--
+
+LOCK TABLES `msg` WRITE;
+/*!40000 ALTER TABLE `msg` DISABLE KEYS */;
+/*!40000 ALTER TABLE `msg` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
 
 DROP TABLE IF EXISTS `organization`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `organization` (
   `idorganization` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -66,40 +144,78 @@ CREATE TABLE `organization` (
   `email_port` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idorganization`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `organization`
+--
 
 LOCK TABLES `organization` WRITE;
-
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
 INSERT INTO `organization` VALUES (1,'Suny ','University at Albany','518-452-0000','Nobody','eugurlu@albany.edu','smtp.gmail.com','icsi518.team5','team5.icsi518','587');
-
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `course`;
+--
+-- Table structure for table `role`
+--
 
-CREATE TABLE `course` (
-  `course_id` int(11) NOT NULL AUTO_INCREMENT,
-  `course_name` varchar(45) NOT NULL,
-  `course_capcity` int(11) NOT NULL,
-  `instructor` int(11) NOT NULL,
-  PRIMARY KEY (`course_id`),
-  UNIQUE KEY `course_id_UNIQUE` (`course_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
-
-
-
-LOCK TABLES `course` WRITE;
-INSERT INTO `course` VALUES (1,'CSI500-Operating System',30,2),(2,'CSI518-Software Engineering',30,2),(3,'CSI535-Aritificial Intelligence',50,4);
-UNLOCK TABLES;
-
-DROP TABLE IF EXISTS 'apt';
-
-CREATE TABLE `apt` (
-  `receiverid` int(11) NOT NULL,
-  `rfisrtname` varchar(45) NOT NULL,
-  `rlastname` varchar(45) NOT NULL,
-  `senderid` int(11) NOT NULL,
-  `sfirstname` varchar(45) NOT NULL,
-  `slastname` varchar(45) NOT NULL,
-  `time` varchar(45) NOT NULL,
-  `date` varchar(45) NOT NULL
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `role` (
+  `idroles` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idroles`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'admin'),(2,'teacher'),(3,'student'),(4,'parent');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin',NULL,NULL,NULL,'password',1),(2,'eu664771','emre','ugurlu','eugurlu@albany.edu','password',3),(3,'pranith','Pranith','Dachepally','pdachepally@albany.edu','1234',3),(4,'john','John','Higgins',NULL,'1234',2),(5,'steven','Steve','Macquire',NULL,'1234',2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-04  4:31:23
